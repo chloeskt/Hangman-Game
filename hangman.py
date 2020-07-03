98d62b8f24d66b67f7e11362647a3cf3f68a4595
chloeskt <chloe.sekkat@ensae.fr>
2020-07-03 18:20:31 +0200

    adding code for letters on screen

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import pygame
+import math as m
 
 #setup display
 pygame.init()
@@ -8,12 +9,32 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game")
 
 #colors
-WHITE = (255, 255, 255)
+BLACK = (0,0, 0)
+WHITE = (255,255,255)
+RED = (255,0, 0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+LIGHT_BLUE = (102,255,255)
 
 #load images
 images = []
 for i in range(7):
     images.append(pygame.image.load("images/hangman" + str(i) + ".png"))
+
+#fonts
+LETTER_FONT = pygame.font.SysFont("arial", 40)
+
+#letters font
+RADIUS = 20
+GAP = 15
+letters = [] #the alphabet
+start_x = round((WIDTH - (GAP + RADIUS*2)*13)/2)
+start_y = 400
+#A = 65
+for i in range(26):
+    x = start_x + GAP*2 + ((RADIUS*2 + GAP) * (i % 13))
+    y = start_y + ((i // 13) * (GAP + RADIUS*2))
+    letters.append([x, y, chr(65 + i), True])
 
 #game variables 
 hangman_status = 0 
@@ -24,20 +45,43 @@ FPS = 60 #max FPS
 clock = pygame.time.Clock()
 run = True 
 
+
+def draw():
+    """
+    Draw the game
+    """
+    win.fill(WHITE)
+
+    for letter in letters: 
+        x, y, char, visible = letter
+        if visible: 
+            pygame.draw.circle(win, BLUE, (x,y), RADIUS, 3)
+            text = LETTER_FONT.render(char, 1, BLACK)
+            win.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
+
+    win.blit(images[hangman_status], dest=(150, 100))
+    pygame.display.update()
+
+
+
 while run:
     #play game 
     clock.tick(FPS)
 
     #win.fill((12, 123, 88))
-    win.fill(WHITE)
-    win.blit(images[hangman_status], dest=(150, 100))
-    pygame.display.update()
+    draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            print(pos)
+            m_x, m_y = pygame.mouse.get_pos()
+            for letter in letters: 
+                x, y, char, visible = letter 
+                if visible:
+                    dis = m.sqrt((x - m_x)**2 + (y - m_y)**2)
+                    if dis < RADIUS:
+                        letter[3] = False 
+            
 
 pygame.quit()
